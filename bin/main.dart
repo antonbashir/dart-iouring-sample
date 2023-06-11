5ded3904bc1b80053c2ff54bd6a32707e110b089
anton.bashirov <anton.sh.local@gmail.com>
2023-06-12 00:21:26 +0300

    samples

--- bin/main.dart
+++ bin/main.dart
@@ -5,6 +5,13 @@ import 'dart:typed_data';
 import 'package:iouring_transport/iouring_transport.dart';
 
 Future<void> main(List<String> args) async {
+  await tcp();
+  await udp();
+  await unixStream();
+  await unixDatagram();
+}
+
+Future<void> tcp() async {
   final transport = Transport();
   final worker = TransportWorker(transport.worker(TransportDefaults.worker()));
   await worker.initialize();
@@ -22,5 +29,72 @@ Future<void> main(List<String> args) async {
   clients.select().writeSingle(Uint8List.fromList("Hello, ".codeUnits));
   await completer.future;
   await transport.shutdown();
-  exit(0);
+}
+
+Future<void> unixStream() async {
+  final transport = Transport();
+  final worker = TransportWorker(transport.worker(TransportDefaults.worker()));
+  await worker.initialize();
+
+  worker.servers.unixStream('/tmp/my_socket', (connection) {
+    connection.stream().listen((event) {
+      connection.writeSingle(Uint8List.fromList((String.fromCharCodes(event.takeBytes()) + "world!").codeUnits));
+    });
+  });
+
+  final completer = Completer();
+  final clients = await worker.clients.unixStream('/tmp/my_socket');
+
+  clients.select().stream().listen((event) {
+    print(String.fromCharCodes(event.takeBytes()));
+    completer.complete();
+  });
+
+  clients.select().writeSingle(Uint8List.fromList("Hello, ".codeUnits));
+  await completer.future;
+  await transport.shutdown();
+}
+
+Future<void> unixDatagram() async {
+  final transport = Transport();
+  final worker = TransportWorker(transport.worker(TransportDefaults.worker()));
+  await worker.initialize();
+
+  worker.servers.unixDatagram('/tmp/server').receive().listen((responder) {
+    responder.respond(Uint8List.fromList((String.fromCharCodes(responder.receivedBytes) + "world!").codeUnits));
+  });
+
+  final completer = Completer();
+  final client = await worker.clients.unixDatagram('/tmp/client', '/tmp/server');
+
+  client.stream().listen((event) {
+    print(String.fromCharCodes(event.takeBytes()));
+    completer.complete();
+  });
+
+  client.send(Uint8List.fromList("Hello, ".codeUnits));
+  await completer.future;
+  await transport.shutdown();
+}
+
+Future<void> udp() async {
+  final transport = Transport();
+  final worker = TransportWorker(transport.worker(TransportDefaults.worker()));
+  await worker.initialize();
+
+  worker.servers.udp(InternetAddress.anyIPv4, 1234).receive().listen((responder) {
+    responder.respond(Uint8List.fromList((String.fromCharCodes(responder.receivedBytes) + "world!").codeUnits));
+  });
+
+  final completer = Completer();
+  final client = await worker.clients.udp(InternetAddress.loopbackIPv4, 5678, InternetAddress.loopbackIPv4, 1234);
+
+  client.stream().listen((event) {
+    print(String.fromCharCodes(event.takeBytes()));
+    completer.complete();
+  });
+
+  client.send(Uint8List.fromList("Hello, ".codeUnits));
+  await completer.future;
+  await transport.shutdown();
 }
